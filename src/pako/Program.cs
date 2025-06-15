148917786998c8243d55e4f8c449190e637eec2b
Cédric Luthi <cedric.luthi@gmail.com>
2025-06-15 22:03:26 +0200

    Simple tool to decode the "pako" payload from mermaid.live urls

---
+++ src/pako/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Buffers.Text;
+using System.IO.Compression;
+using System.Text.Json;
+using TextCopy;
+
+var uri = new Uri(ClipboardService.GetText() ?? "about:blank");
+var pako = Base64Url.DecodeFromChars(uri.Fragment.AsSpan()[6..]);
+
+using var memoryStream = new MemoryStream(pako);
+using var zlibStream = new ZLibStream(memoryStream, CompressionMode.Decompress, leaveOpen: true);
+using var json = JsonDocument.Parse(zlibStream);
+var pretty = JsonSerializer.Serialize(json, new JsonSerializerOptions { WriteIndented = true });
+Console.WriteLine(pretty);
