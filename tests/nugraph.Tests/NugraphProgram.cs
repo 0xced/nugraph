88354f0cc70b936229618e7af6e360a31fcf18ed
Cédric Luthi <cedric.luthi@gmail.com>
2025-06-23 23:22:59 +0200

    Debug LegacyConsoleBackend.SetStyle

--- tests/nugraph.Tests/NugraphProgram.cs
+++ tests/nugraph.Tests/NugraphProgram.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using System.Threading.Tasks;
 using NuGet.Common;
 using NuGet.Versioning;
+using Spectre.Console;
 using Spectre.Console.Testing;
 
 namespace nugraph.Tests;
@@ -19,10 +20,22 @@ public sealed class NugraphProgram : Nugraph
 
     public override async Task<(int ExitCode, string StdOut, string StdErr)> RunAsync(string[] arguments, string? workingDirectory = null, LogLevel logLevel = LogLevel.Warning, bool noBrowser = true)
     {
-        using var consoleOut = new TestConsole();
-        consoleOut.Profile.Width = 256;
-        using var consoleErr = new TestConsole();
-        consoleErr.Profile.Width = 256;
+        var consoleOut = AnsiConsole.Create(new AnsiConsoleSettings
+        {
+            Ansi = AnsiSupport.No,
+            ColorSystem = ColorSystemSupport.Detect,
+            Out = new AnsiConsoleOutput(Console.Out),
+        });
+        var consoleErr = AnsiConsole.Create(new AnsiConsoleSettings
+        {
+            Ansi = AnsiSupport.No,
+            ColorSystem = ColorSystemSupport.Detect,
+            Out = new AnsiConsoleOutput(Console.Error),
+        });
+        // using var consoleOut = new TestConsole();
+        // consoleOut.Profile.Width = 256;
+        // using var consoleErr = new TestConsole();
+        // consoleErr.Profile.Width = 256;
         await using var stdOut = new StringWriter();
         var program = new Program(new DirectoryInfo(workingDirectory ?? Environment.CurrentDirectory), consoleOut, consoleErr, stdOut);
         var args = arguments.Append("--log").Append(logLevel.ToString()).Append("--no-browser").Append(noBrowser.ToString()).ToArray();
@@ -32,9 +45,14 @@ public sealed class NugraphProgram : Nugraph
 
     public override string Version { get; }
 
-    private static string GetOutput(TestConsole console, StringWriter? writer)
+    private static string GetOutput(IAnsiConsole console, StringWriter? writer)
     {
-        var lines = (console.Output + writer).TrimEnd();
-        return lines;
+        if (console is TestConsole testConsole)
+        {
+            var lines = (testConsole.Output + writer).TrimEnd();
+            return lines;
+        }
+
+        return "";
     }
 }
