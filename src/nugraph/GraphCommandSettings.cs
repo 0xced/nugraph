710371e2a20065cf347cef4f1dfb20741dbd59f5
Cédric Luthi <cedric.luthi@gmail.com>
2026-09-16 15:49:09 +0200

    Remove unnecessary interpolated strings

--- src/nugraph/GraphCommandSettings.cs
+++ src/nugraph/GraphCommandSettings.cs
@@ -59,9 +59,9 @@ internal sealed class GraphCommandSettings : CommandSettings
     public FileInfo? OutputFile { get; init; }
 
     [CommandOption("-m|--format <FORMAT>")]
-    [Description($"The format to use when the [b]--output[/] option is not specified\n" +
-                 $"Use [b]mmd[/] or [b]mermaid[/] for Mermaid Live Editor https://mermaid.live\n" +
-                 $"Use [b]dot[/], [b]gv[/] or [b]graphviz[/] for Edotor https://edotor.net\n" +
+    [Description("The format to use when the [b]--output[/] option is not specified\n" +
+                 "Use [b]mmd[/] or [b]mermaid[/] for Mermaid Live Editor https://mermaid.live\n" +
+                 "Use [b]dot[/], [b]gv[/] or [b]graphviz[/] for Edotor https://edotor.net\n" +
                  "See https://github.com/0xced/nugraph/#output for even more supported formats")]
     [DefaultValue("mermaid")]
     public string Format { get; init; } = "";
